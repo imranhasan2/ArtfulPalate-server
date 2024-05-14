b2392eada1a1c6a490be2c7b64063de6b06ffd91
imranhasan2 <imranhasan030303@gmail.com>
2024-05-15 00:42:52 +0600

    get single id from data base by query

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -44,6 +44,13 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/addFood/:id',async(req,res) =>{
+            const id = req.params.id;
+            const query ={_id : new ObjectId (id)}
+            const result = await foodCollection.findOne(query)
+            res.send(result)
+
+        })
 
 
         // Send a ping to confirm a successful connection
